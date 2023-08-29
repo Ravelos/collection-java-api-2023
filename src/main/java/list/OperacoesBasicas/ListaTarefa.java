24f19d79f56e319d08e2e89a4474ea00f577dc5a
Oswaldo Ravelo <raveloswaldo@gmail.com>
2023-08-28 21:33:36 -0300

    Tests ListaTarefas methods

--- src/main/java/list/OperacoesBasicas/ListaTarefa.java
+++ src/main/java/list/OperacoesBasicas/ListaTarefa.java
@@ -32,4 +32,17 @@ public class ListaTarefa {
     public void obterDescricoesTarefas(){
         System.out.println(tarefaList);
     }
+
+    public static void main(String[] args) {
+        ListaTarefa listaTarefa = new ListaTarefa();
+        System.out.println("O número total de elementos na lista é: " + listaTarefa.obterNumeroTotalTarefas());
+        listaTarefa.adicionarTarefa("Tarefa 1");
+        listaTarefa.adicionarTarefa("Tarefa 1");
+        listaTarefa.adicionarTarefa("Tarefa 2");
+
+        System.out.println("O número total de elementos na lista é: " + listaTarefa.obterNumeroTotalTarefas());
+        listaTarefa.removerTarefa("Tarefa 2");
+        System.out.println("O número total de elementos na lista é: " + listaTarefa.obterNumeroTotalTarefas());
+        listaTarefa.obterDescricoesTarefas();
+    }
 }
--- src/main/java/list/OperacoesBasicas/Tarefa.java
+++ src/main/java/list/OperacoesBasicas/Tarefa.java
@@ -10,4 +10,9 @@ public class Tarefa {
     public String getDescricao() {
         return descricao;
     }
+
+    @Override
+    public String toString() {
+        return  descricao;
+    }
 }
