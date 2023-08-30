144c2ff697ec15a174561b384d5e65fc64e3e8a9
Oswaldo Ravelo <raveloswaldo@gmail.com>
2023-08-29 22:46:18 -0300

    Creates pesquisa package and class livro

--- src/main/java/list/OperacoesBasicas/CarrinhoDeCompras.java
+++ src/main/java/list/OperacoesBasicas/CarrinhoDeCompras.java
@@ -4,8 +4,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CarrinhoDeCompras {
-    private List<Item>itemList;
-    public CarrinhoDeCompras(){ this.itemList=new ArrayList<>();
-    } public void adicionarItem(String nome, int quantidade, double preco){ itemList.add(new Item(nome,quantidade,preco));
+    private List<Item> itemList;
+
+    public CarrinhoDeCompras() {
+        this.itemList = new ArrayList<>();
     }
+
+    public void adicionarItem(String nome, int quantidade, double preco) {
+        itemList.add(new Item(nome, quantidade, preco));
+    }
+
 }
