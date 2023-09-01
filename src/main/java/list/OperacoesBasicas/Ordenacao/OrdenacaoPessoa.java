45664aafa17f95dfce3caf0b44516cc8850f66f5
Oswaldo Ravelo <raveloswaldo@gmail.com>
2023-09-01 11:04:33 -0300

    Sorts using comparator and comparable

---
+++ src/main/java/list/OperacoesBasicas/Ordenacao/OrdenacaoPessoa.java
@@ -0,0 +1,31 @@
+package main.java.list.OperacoesBasicas.Ordenacao;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class OrdenacaoPessoa {
+//    atributo
+    private List<Pessoa> pessoaList;
+
+    public OrdenacaoPessoa() {
+        this.pessoaList = new ArrayList<>();
+    }
+
+    public void adicionarPessoas(String nome, int idade, double altura){
+        pessoaList.add(new Pessoa(nome, idade, altura));
+    }
+
+    public List<Pessoa> ordenarPorIdade(){
+        List<Pessoa> pessoasPorIdade = new ArrayList<>(pessoaList);
+        Collections.sort(pessoasPorIdade);
+        return pessoasPorIdade;
+    }
+
+    public List<Pessoa> ordenarPorAltura(){
+        List<Pessoa> pessoasPorAltura = new ArrayList<>();
+        Collections.sort(pessoasPorAltura,new ComparatorPorAltura());
+        return pessoasPorAltura;
+    }
+
+}
