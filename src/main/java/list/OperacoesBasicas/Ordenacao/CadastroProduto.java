ac9389a7bc6c985f3507de6c0c361fcee4bd2692
Oswaldo Ravelo <raveloswaldo@gmail.com>
2023-09-03 18:15:31 -0300

    Tests the methods

--- src/main/java/list/OperacoesBasicas/Ordenacao/CadastroProduto.java
+++ src/main/java/list/OperacoesBasicas/Ordenacao/CadastroProduto.java
@@ -1,4 +1,46 @@
 package main.java.list.OperacoesBasicas.Ordenacao;
 
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
 public class CadastroProduto {
+    private Set<Produto> produtoSet;
+
+    public CadastroProduto() {
+        this.produtoSet = new HashSet<>();
+    }
+
+    public static void main(String[] args) {
+        CadastroProduto cadastroProduto = new CadastroProduto();
+        cadastroProduto.adicionarProduto(1, "lavalouça", 1.22, 1);
+        cadastroProduto.adicionarProduto(2, "monitor", 3000, 40);
+        cadastroProduto.adicionarProduto(3, "celular", 100.22, 50);
+        cadastroProduto.adicionarProduto(4, "tv", 21.22, 60);
+        cadastroProduto.adicionarProduto(5, "sapato", 31.22, 70);
+        System.out.println(cadastroProduto.produtoSet);
+        System.out.println(cadastroProduto.exibirPorPreco());
+        System.out.println(cadastroProduto.exibirProdutosPorNome());
+//        cadastroProduto.exibirPorPreco();
+//        cadastroProduto.exibirProdutosPorNome();
+//        cadastroProduto.exibirPorPreco();
+
+    }
+
+    public void adicionarProduto(long cod, String nome, double preco, int quantidade) {
+        produtoSet.add(new Produto(nome, cod, preco, quantidade));
+    }
+
+    public Set<Produto> exibirProdutosPorNome() {
+        Set<Produto> produtosPorNome = new TreeSet<>(produtoSet);
+        return produtosPorNome;
+    }
+
+    public Set<Produto> exibirPorPreco() {
+        Set<Produto> produtosPorPreco = new TreeSet<>(new ComparatorPorPreco());
+        produtosPorPreco.addAll(produtoSet);
+        return produtosPorPreco;
+    }
+
 }
--- src/main/java/list/OperacoesBasicas/Ordenacao/Produto.java
+++ src/main/java/list/OperacoesBasicas/Ordenacao/Produto.java
@@ -1,8 +1,9 @@
 package main.java.list.OperacoesBasicas.Ordenacao;
 
+import java.util.Comparator;
 import java.util.Objects;
 
-public class Produto {
+public class Produto implements Comparable<Produto> {
     private String nome;
     private long codigo;
     private double preco;
@@ -51,5 +52,18 @@ public class Produto {
                 ", preco=" + preco +
                 ", quantidade=" + quantidade +
                 '}';
+    }
+
+    @Override
+    public int compareTo(Produto p) {
+        return nome.compareToIgnoreCase(p.getNome());
+    }
+}
+
+    class ComparatorPorPreco implements Comparator<Produto> {
+
+    @Override
+    public int compare(Produto p1, Produto p2) {
+        return Double.compare(p1.getPreco(), p2.getPreco());
     }
 }
